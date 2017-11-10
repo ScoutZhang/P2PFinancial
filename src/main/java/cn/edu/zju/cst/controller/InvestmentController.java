78a2bc54418e8e45e4bd98ff1ddeb2271f8105e2
scout <229249847@qq.com>
2017-11-10 16:29:19 +0800

    修正每日还本付息计算bug

--- src/main/java/cn/edu/zju/cst/controller/InvestmentController.java
+++ src/main/java/cn/edu/zju/cst/controller/InvestmentController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 /**
  * Created by jolivan on 2017/11/6.
